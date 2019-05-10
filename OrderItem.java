359e7491cdd6f67551372fcb97ff7b1ff211f11c
swati2805 <47091851+swati2805@users.noreply.github.com>
2019-05-10 13:23:44 -0700

    Updated and changed OrderItem.java

--- OrderItem.java
+++ OrderItem.java
@@ -1,4 +1,4 @@
-package Starbucks;
+package starbucks;
 import java.util.List;
 
 public class OrderItem {
@@ -6,8 +6,15 @@ public class OrderItem {
 	private String itemCode;
 	private String pickUpLoc;
 	private String size;
-	private List addOns;
+//	private List addOns;
+	private String addOns;
 	
+	public String getAddOns() {
+		return addOns;
+	}
+	public void setAddOns(String addOns) {
+		this.addOns = addOns;
+	}
 	public String getItemCode() {
 		return itemCode;
 	}
@@ -26,33 +33,13 @@ public class OrderItem {
 	public void setSize(String size) {
 		this.size = size;
 	}
-	public List getAddOns() {
-		return addOns;
-	}
-	public void setAddOns(List addOns) {
-		this.addOns = addOns;
-	}
+//	public List getAddOns() {
+//		return addOns;
+//	}
+//	public void setAddOns(List addOns) {
+//		this.addOns = addOns;
+//	}
 	
 	
-	
-//	enum Item 
-//	{ 
-//	    Cof1, Cof2, Frap1, Frap2 ;
-//	} 
-//	
-//	enum Location 
-//	{ 
-//	    Downtown, RoseGarden, Airport ;
-//	} 
-//	
-//	enum Size 
-//	{ 
-//	    L, M, S ;
-//	} 
-//	
-//	enum AddOns 
-//	{ 
-//	    Cream, Caramel, expresso ;
-//	} 
 	
 }
